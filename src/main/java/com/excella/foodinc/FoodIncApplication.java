dfb1e5a4c62dad240d59cd94a500289c71cc535c
Kevin Ellis <kevin.ellis@excella.com>
2019-01-31 14:11:10 -0500

    initial commit

---
+++ src/main/java/com/excella/foodinc/FoodIncApplication.java
@@ -0,0 +1,14 @@
+package com.excella.foodinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodIncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodIncApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/excella/foodinc/FoodIncApplicationTests.java
@@ -0,0 +1,17 @@
+package com.excella.foodinc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodIncApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
