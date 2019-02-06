e28284e44480f405c0baeab7f1b5df1c68d47857
Will Krause <wkrause1@radford.edu>
2019-02-06 12:45:21 -0500

    Using Mono now

--- src/main/java/com/excella/foodinc/FoodIncController.java
+++ src/main/java/com/excella/foodinc/FoodIncController.java
@@ -1,13 +1,18 @@
 package com.excella.foodinc;
 
+import org.reactivestreams.Publisher;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class FoodIncController {
 
     @RequestMapping("/demo")
-    public String index() {
-        return "Hello World";
+    public Publisher<String> index() {
+         Mono<String> helloWorld = Mono.just("Hello World");
+         return helloWorld;
+        }
     }
-}
